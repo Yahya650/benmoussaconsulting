7fe43c33acc274b59e270a1bfc8f49fc494e92be
yahya <Yahyahamdi650@gmail.com>
2024-08-10 13:39:17 +0100

    changes

--- app/Http/Controllers/v1/web/HomeController.php
+++ app/Http/Controllers/v1/web/HomeController.php
@@ -8,19 +8,19 @@ class HomeController extends Controller
 {
     public function index()
     {
-        return view('v1.web.index');
-        // return "500";
+        // return view('v1.web.index');
+        return "500";
     }
 
     public function contact()
     {
-        return view('v1.web.pages.contact');
-        // return "500";
+        // return view('v1.web.pages.contact');
+        return "500";
     }
 
     public function coachingTypes()
     {
-        return view('v1.web.pages.coaching-types');
-        // return "500";
+        // return view('v1.web.pages.coaching-types');
+        return "500";
     }
 }
